6c610985d18fffdf1f8d11612c215b0a35745401
Kiefer Yap <kiefer.yap@gmail.com>
2016-03-30 23:43:12 +0900

    So it begins.

---
+++ BudgetBunny/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BudgetBunny
+//
+//  Created by Kiefer Yap on 3/30/16.
+//  Copyright © 2016 Kiefer Yap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BudgetBunny/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BudgetBunny
+//
+//  Created by Kiefer Yap on 3/30/16.
+//  Copyright © 2016 Kiefer Yap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
