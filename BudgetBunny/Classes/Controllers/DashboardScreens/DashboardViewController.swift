d7c663cb7f3f39afa0a877293b4a57c2e248915a
Kiefer Yap <kiefer.yap@gmail.com>
2016-04-13 16:11:36 +0900

    [GEN-0006] Added view controller files and linked them to the storyboard

---
+++ BudgetBunny/Classes/Controllers/DashboardScreens/DashboardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DashboardViewController.swift
+//  BudgetBunny
+//
+//  Created by Kiefer Yap on 4/13/16.
+//  Copyright © 2016 Kiefer Yap. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
