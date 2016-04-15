88117fd08023358a42e6729f675875d3719f4d3c
Kiefer Yap <kiefer.yap@gmail.com>
2016-04-15 23:49:51 +0900

    [ACC-0001] Added colors

---
+++ BudgetBunny/Classes/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+struct Constants {
+    struct Colors {
+        static let NormalGreen = "#00B16A"
+        static let LightGreen = "#3FC380"
+        static let DarkGreen = "#16A085"
+        static let DangerColor = "#EC644B"
+    }
+}
